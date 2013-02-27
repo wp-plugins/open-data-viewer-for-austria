99b1cd6b6110c550594956271c1fc8832baa8d33
ondics <ondics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-27 15:26:44 +0000

    none
    git-svn-id: https://plugins.svn.wordpress.org/open-data-viewer-for-austria/trunk@674102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- proxy.php
+++ proxy.php
@@ -1,17 +1,13 @@
 <?php
+require( '../../../wp-load.php' );
+
+
 // this proxy is needed because to get the ckan data from a foreign server
 $url = urldecode(($_GET['url']));
 $url = str_replace(" ","%20",$url);
 if (strpos($url, "pop") !== FALSE) return ""; // http://www.heise.de/newsticker/meldung/cURL-auf-Abwegen-1800433.html
-$ch = curl_init();
-// URL to grab
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
-$output = curl_exec($ch);
-curl_close($ch);
+
+$output = wp_remote_retrieve_body(wp_remote_get($url, array("user-agent" => "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0"))); // using a common useragent to prevent blocking from some server
 
 // this code converts to utf-8 when needed. But mb_check_encoding is critical code! http://de2.php.net/manual/de/function.mb-check-encoding.php
 if(!mb_check_encoding($output, 'UTF-8')) { // only convert when string is not utf-8
