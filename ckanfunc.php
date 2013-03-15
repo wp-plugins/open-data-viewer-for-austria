d773c0ea64f0141859e0a022ee65eb30e4fb2098
ondics <ondics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-15 17:08:06 +0000

    Versionnumber
    git-svn-id: https://plugins.svn.wordpress.org/open-data-viewer-for-austria/tags/1.0.6@682507 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ckanfunc.php
+++ ckanfunc.php
@@ -3,7 +3,7 @@
 Plugin Name: Open Data Viewer for Austria
 Plugin URI: http://apps4austria-open-data.ondics.de
 Description: Open Data Viewer for Austria brings the full power of open data from Austria to your Wordpress site.
-Version: 1.0.5
+Version: 1.0.6
 Author: Ondics GmbH
 Author URI: http://ondics.de
 License: None
